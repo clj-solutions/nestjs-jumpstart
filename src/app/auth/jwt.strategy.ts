43d5dcb60ac8a5ddcb61b9e5c81113cbb713b778
wjone <engel0088@gmail.com>
2020-01-08 19:27:06 +0900

    fix: ormconfig

--- src/app/auth/jwt.strategy.ts
+++ src/app/auth/jwt.strategy.ts
@@ -13,7 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     })
   }
 
-  async validate(payload: any): any {
+  async validate(payload: any): Promise<any> {
     return {
       uid: payload.sub,
       email: payload.email,
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './user.controller'
-import { User } from './user.entity'
 import { UserService } from './users.service'
+import { User } from './user.entity'
 
 @Module({
   imports: [
