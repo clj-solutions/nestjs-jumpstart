c74dcf3a15f8c88dd930a3ce1132db45b0a536c7
wjone <engel0088@gmail.com>
2020-08-03 12:19:31 +0900

    test: auth.controller.spec init

--- src/app/auth/auth.controller.spec.ts
+++ src/app/auth/auth.controller.spec.ts
@@ -1,18 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { Logger, LoggerService } from '@nestjs/common';
+import { User } from '../user/user.entity';
+import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller';
+import { Repository } from 'typeorm';
+import factory from '../user/user.factory';
+import { AuthRegisterDto } from './dto/auth.register.dto';
+import { AuthTokenDto } from './dto/auth.token.dto'
+import { JWT_SECRET } from './secret.key';
+import { TYPEORM_MODULE_OPTIONS } from '../../config/database';
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  const repository: Repository<User> = new Repository<User>();
+  const service: AuthService = new AuthService(new JwtService({
+    secret: JWT_SECRET,
+    signOptions: { expiresIn: '3h' }
+  }), repository);
+  const controller: AuthController = new AuthController(service, new Logger());
+  const repositoryUser = [];
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController],
-    }).compile();
+    jest.clearAllMocks();
 
-    controller = module.get<AuthController>(AuthController);
   });
 
   it('should be defined', () => {
+    expect(service).toBeDefined();
     expect(controller).toBeDefined();
   });
+
+  describe('should register a user with valid params', () => {
+    const user = factory.build();
+  });
+
 });
